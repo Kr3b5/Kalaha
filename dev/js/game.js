d705d0bcce9ce29587971dd2ed1f15cab2c4d670
Kr3b5 <codersuniverse42@gmail.com>
2020-11-07 16:18:44 +0100

    Add NewGame

--- dev/js/game.js
+++ dev/js/game.js
@@ -7,3 +7,23 @@
  * @author Kevin Lempert
  * @author Jonas Renz
  */
+
+
+
+function newGame(){
+    //Reset Pits 
+    var pits = document.getElementsByClassName("pit");
+    for (var i = 0; i < pits.length; i++) {
+        pits[i].childNodes[0].innerText = "4";
+    }
+
+    //Reset Finish
+    var pits = document.getElementsByClassName("finish");
+    for (var i = 0; i < pits.length; i++) {
+        pits[i].childNodes[0].innerText = "0";
+    }
+
+    //Reset Player 
+    document.getElementById("player").innerText="1";
+    //TODO: SET Player One in JS
+}
